eaaf94bd24b12c2f86628cccb503b32bd8fc3641
Ariel <asolmon88@gmail.com>
2024-12-09 11:00:44 -0600

    pedidos

--- src/data/orders.ts
+++ src/data/orders.ts
@@ -7,60 +7,42 @@ import { Order } from "../types/order";
 export const orders: Order[] = [
   {
     id: 1,
-    projectName: "Camisas navideñas",
-    dueDate: "8 de diciembre",
+    projectName: "Camisas NCSC",
+    dueDate: "14 de diciembre",
     designLink: "https://drive.google.com/drive/folders/1WqohOrBVryMpcVgDsuHeLjbVz0s6HJQU?usp=drive_link",
     designSize:"Por definir",
     quantities: {
       Blanco: {
         sizes: {
-          S: 2,
-          M: 4,
-          L: 1,
-        },
-        ready: true,
-      },
-      Rojo: {
-        sizes: {
-          S: 39,
-          M: 36,
-          L: 18,
-          XL: 3,
-        },
-        ready: false,
-      },
-      Verde: {
-        sizes: {
-          XS: 1,
-          S: 3,
-          M: 5,
-          L: 1,
+          S: 48,
+          M: 48,
+          L: 48,
           XL: 1
-        },
-        ready: true,
-      },
-    },
-    colors: ["Blanco", "Rojo", "Verde"],
-    totalAmount: 120, // Sum of all sizes across all colors
-  },
-  {
-    id: 2,
-    projectName: "Camisas grises",
-    dueDate: "Lo antes posible",
-    designLink: "https://drive.google.com/drive/folders/15FU7zeXXu45nI425oKI4JsA9EHfgDA7X?usp=drive_link",
-    designSize: "10x10 adelante; 30x30 atrás",
-    quantities: {
-      Gris: {
-        sizes: {
-          M: 20,
-          L: 28,
-          XXL: 2,
         },
         ready: true,
       }
     },
-    colors: ["Gris"],
-    totalAmount: 50, // Sum of all sizes across all colors
-  },
+    colors: ["Blanco"],
+    totalAmount: 145, // Sum of all sizes across all colors
+  }
+  // {
+  //   id: 2,
+  //   projectName: "Camisas grises",
+  //   dueDate: "Lo antes posible",
+  //   designLink: "https://drive.google.com/drive/folders/15FU7zeXXu45nI425oKI4JsA9EHfgDA7X?usp=drive_link",
+  //   designSize: "10x10 adelante; 30x30 atrás",
+  //   quantities: {
+  //     Gris: {
+  //       sizes: {
+  //         M: 20,
+  //         L: 28,
+  //         XXL: 2,
+  //       },
+  //       ready: true,
+  //     }
+  //   },
+  //   colors: ["Gris"],
+  //   totalAmount: 50, // Sum of all sizes across all colors
+  // },
   // Add more orders as needed
 ];
